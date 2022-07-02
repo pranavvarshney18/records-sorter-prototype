64daefdf3e33cdef7d7e0e4f1de2407d6cc872a2
Pranav Varshney <pranavvarshney18@gmail.com>
2022-07-02 12:55:46 +0530

    add static files and express-ejs-layout

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ const port = 8000;
 //setting a fixed layout for every view
 const expressLayouts = require("express-ejs-layouts");
 app.use(expressLayouts);
+//adding css and js files from layout.ejs to required ejs files
+app.set("layout extractStyles", true);
+app.set("layout extractScripts", true);
+//include static files
+app.use(express.static("./assets"));
 
 //set path for routes
 app.use("/", require("./routes/index"));
